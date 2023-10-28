eeb1875134fc4baa2931fa73af1dd8266708ce44
izzetcansuzen <izzetcansuzen@gmail.com>
2023-10-28 18:52:16 +0300

    added box shadow 8xl to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/flowbite-react/**/*.js",
     "./public/**/*.html",
   ],
   theme: {
     extend: {
-
+      boxShadow: {
+        "8xl": "0 35px 60px -15px rgba(0, 0, 0, 0.8)",
+      },
     },
   },
-  plugins: [
-    require("flowbite/plugin")
-  ],
-}
+  plugins: [require("flowbite/plugin")],
+};
