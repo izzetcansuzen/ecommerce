75d5306d054d40c1ddca7cf5ba493f26554abd6e
izzetcansuzen <izzetcansuzen@gmail.com>
2023-10-28 12:47:25 +0300

    changed product card responsiveness

--- app/page.js
+++ app/page.js
@@ -42,7 +42,7 @@ export default function Home() {
       </div>
       <div>
         <h2 className="font-bold text-xl md:text-2xl">Best Products</h2>
-        <div className="w-[155px] h-[260px] bg-[#25293C] rounded-md relative">
+        <div className="w-[155px] bg-[#25293C] rounded-md relative md:w-[200px] md:h-full">
           <div className="w-full h-[130px] overflow-hidden rounded-tl-md rounded-tr-md mb-1">
             <Image
               src="/images/profile.png"
@@ -52,14 +52,18 @@ export default function Home() {
               className="object-cover"
             ></Image>
           </div>
-          <div className="p-1 flex flex-col gap-2">
+          <div className="p-1 flex flex-col gap-2 h-full">
             <div className="flex flex-col">
-              <div className="text-white text-[12px]">Iphone 12</div>
-              <div className="text-gray-500 text-[10px]">Apple</div>
+              <div className="text-white text-[12px] md:text-[22px]">
+                Iphone 12
+              </div>
+              <div className="text-gray-500 text-[10px] md:text-[12px]">
+                Apple
+              </div>
             </div>
             <Rating>
               <Rating.Star />
-              <p className="text-[10px] text-white">4.95</p>
+              <p className="text-[10px] text-white md:text-[12px]">4.95</p>
               <span className="mx-1.5 h-1 w-1 rounded-full bg-gray-500 dark:bg-gray-400" />
               <a
                 className="text-[10px] font-medium text-white underline hover:no-underline dark:text-white"
@@ -68,9 +72,9 @@ export default function Home() {
                 <p>73 reviews</p>
               </a>
             </Rating>
-            <div className="text-[12px] text-white">$299</div>
+            <div className="text-[12px] text-white md:text-[20px]">$299</div>
           </div>
-          <Button className="absolute bottom-0 left-0 text-[10px] w-full h-[25px] rounded-md rounded-tl-none rounded-tr-none bg-[#7367F0] enabled:hover:bg-[#7367F0] focus:ring-0 border-none focus:bg-[#7367F0] outline-none">
+          <Button className="sticky bottom-0 left-0 text-[10px] w-full h-[25px] rounded-md rounded-tl-none rounded-tr-none bg-[#7367F0] enabled:hover:bg-[#7367F0] focus:ring-0 border-none focus:bg-[#7367F0] outline-none">
             Add To Cart
           </Button>
         </div>
